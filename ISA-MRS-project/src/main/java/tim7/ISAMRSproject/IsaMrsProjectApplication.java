3f3a760d165adac741c12c819080d6082c514aa5
GajicMladen <102261584+GajicMladen@users.noreply.github.com>
2022-04-07 19:15:06 +0200

    First commit

---
+++ ISA-MRS-project/src/main/java/tim7/ISAMRSproject/IsaMrsProjectApplication.java
@@ -0,0 +1,13 @@
+package tim7.ISAMRSproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaMrsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaMrsProjectApplication.class, args);
+	}
+
+}
