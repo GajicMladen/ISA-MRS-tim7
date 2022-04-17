eb28cddda1ad428f174d7e4045a80b7a660e5e4c
GajicMladen <102261584+GajicMladen@users.noreply.github.com>
2022-04-17 20:14:30 +0200

    0014 - Kreirati kostur stranice za vlasnika vikendice

--- ISA-MRS-project/go-fishing-ui/go-fishing/src/app/features/cottage/cottage.module.ts
+++ ISA-MRS-project/go-fishing-ui/go-fishing/src/app/features/cottage/cottage.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CottageProfilepageComponent } from './components/cottage-profilepage/cottage-profilepage.component';
+import { CottageOwnerpageComponent } from './components/cottage-ownerpage/cottage-ownerpage.component';
 
 
 
 @NgModule({
   declarations: [
-    CottageProfilepageComponent
+    CottageProfilepageComponent,
+    CottageOwnerpageComponent
   ],
   imports: [
     CommonModule
